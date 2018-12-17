7bc551c288597f50853341b1a5ed7309fd5894bb
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-12-17 02:15:11 -0800

    enabled publishing of build/ directory to repo

---
+++ build/precache-manifest.616a4f0d19ccd410af076a32243fdc4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d09df6b9a6b6c18a37",
+    "url": "/static/css/main.f61fb9d2.chunk.css"
+  },
+  {
+    "revision": "d9d09df6b9a6b6c18a37",
+    "url": "/static/js/main.d9d09df6.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4c1ffca0438f590ec4803a856dadeae",
+    "url": "/index.html"
+  }
+];
